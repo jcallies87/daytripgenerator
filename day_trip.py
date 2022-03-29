426ced1f4ca36a08eaaf6991b00e4041184ba29a
Justin Callies <jcallies@gmail.com>
2022-03-29 13:28:33 -0500

    edited while loop day_trip_planner

--- day_trip.py
+++ day_trip.py
@@ -1,53 +1,61 @@
-destinations = ['Houston', 'Wichita', 'Milwaukee', 'New York City']
+print("Hello welcome to the day trip planner.")
+day_trip_planner = True
+while (day_trip_planner):
+    import random
+    def random_generator(list_one):
+        for vehicle in list_one:
+            variable = len(list_one)
+            vehicle = (random.randrange(0, variable))
+            product_one = list_one.pop(vehicle)
+            variable = len(list_one) - 1
+            return product_one
+    location = ["Houston", 'Wichita', 'Milwaukee', 'New York City']
+    list_one = location
+    while location:
+        city = random_generator(list_one)
+        greeting = input(f"Would you like to start your trip in {city} ? y/n ")
+        if greeting == ("y"):
+            break
+        else:
+            print("Okay lets try somewhere else?")
+    product_one = city
 
-#houston_restaurants = ['Kings BBQ', 'Whataburger', 'Mamacitas mexican restaurant', 'Killens BBQ']
-#houston_entertainment = ['Tour Buffalo Bayou Park Cistern', 'Play Top Golf', 'NASA Space Center','Kemah Broadwalk']
+    resturants = ['Whataburger', 'Olive Garden', 'Ruby Tuesday', 'Taco Bell']
+    list_one = resturants
+    while resturants:
+        dining = random_generator(list_one)
+        where_to_eat = input(f"Would you like to have dinner at {dining} tonight ? y/n ")
+        if where_to_eat == ("y"):
+            break
+        else:
+            print("Okay lets try somewhere else?")
+    product_one = dining
 
-#wichita_resturants = ['Hog Wild Pit BBQ', 'Bomber Burger', 'Spangels', 'Fredddys Frozen Custard']
-#wichita_entertainment = ['Wichita Wingnuts game', 'Wichita riverwalk', 'Club Rodeo', 'Sedgwick County Zoo']
+    mode_of_transport = ['the train', 'the car', 'the bus', 'walking']
+    list_one = mode_of_transport
+    while mode_of_transport:
+        transport = random_generator(list_one)
+        means_of_travel = input(f"would you like to use {transport} as your mode of transportation? y/n ")
+        if means_of_travel == ("y"):
+            break
+        else:print("Okay lets try something else")
+    product_one = transport
 
-#milwaukee_resturants = ['Kopps', 'Oscars Frozen Custard' 'AJ Bombers', 'Milwaukee Burger Company']
-#milwaukee_entertainment = ['Harley-Davison Museum', 'Concert at Eagles Ballroom', 'Brewers game', 'Potawatomi Casino']
+    entertainment = ['go to a movie at the movie theater', 'see a concert', 'visit the zoo', 'enjoy the museum']
+    list_one = entertainment
+    while entertainment:
+        activity = random_generator(list_one)
+        what_to_see = input(f"would you like to {activity} as your activity? y/n ")
+        if what_to_see == ("y"):
+            break
+        else:print("Okay lets try something else")
+    product_one = activity
 
-#nyc_resturants = ['Grand Central Oyster Bar', 'Tavern on the Green', 'Rosas Pizza', 'Royal 35 Steakhouse']
-#nyc_entertainment = ['Tour Empire State Bulding', 'Tour Central Park', '9/11 Memorial', 'show on Broadway']
-
-
-import random
-#def destinations_random(destinations):
-#    for pick in destinations:
-#        destinations = (random.randint(0, 3))
-#        if destinations == (0):
-#            return 'Houston'
-#        elif destinations == (1):
-#            return 'Wichita'
-#        elif destinations == (2):
-#            return 'Milwaukee'
-#        elif destinations == (3):
-#            return 'New York City'
-
-#location = destinations_random(destinations)
-#print("Hello welcome to the day trip planner.")
-#while location:
-#    city = destinations_random(destinations)
-#    greeting = input(f"Would you like to start your trip in {city} ? y/n ")
-#    if greeting == ("y"):
-#        break
-#    else:
-#        print("Okay lets try somewhere else?")
-
-mode_of_transport = ['train', 'car', 'bus', 'walk']
-def mode_of_movement(mode_of_transport):
-    for vehicle in mode_of_transport:
-        variable = len(mode_of_transport)
-        vehicle = (random.randrange(0, variable))
-        transport = mode_of_transport.pop(vehicle)
-        variable = len(mode_of_transport) - 1
-        return transport
-while mode_of_transport:
-    transport = mode_of_movement(mode_of_transport)
-    means_of_travel = input(f"would you like to use {transport} as your mode of transportation? y/n ")
-    if means_of_travel == ("y"):
-        break
-    else:print("Okay lets try something else")
-
+    print(f"So your day trip is set to take place in {city}. We will get there via {transport}. We will be dining at {dining} tonight, and we are planning to {activity}.")
+    confirm = input("Do you want to take this day trip? y/n ")
+    for day_trip_planner in confirm:
+        if confirm == "y":
+            print("Thank you for using the day trip planner, enjoy your trip.")
+            day_trip_planner = False
+        elif print( "ok lets try this again"):
+            break
